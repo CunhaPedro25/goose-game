c9f13bc714fdef41a62f4fc96a499071a346cee8
Pedro Cunha <pedro10cunha@gmail.com>
2023-06-04 04:52:11 +0100

    Created main menu

--- src/main.c
+++ src/main.c
@@ -9,14 +9,32 @@
 #include "questions/questions.h"
 #include "utils/ui.h"
 #include "utils/functions.h"
+#include "utils/menus.h"
+
+void settings(){
+  printf("%d", getNumberMultipleChoiceQuestions());
+
+  waitInput("...");
+}
 
 int main(){
   clearConsole();
 
+  int selection;
+
   installGame();
-  gameLoop();
+
+  do{
+    MenuOptions options[] = {
+      {"Play", gameLoop},
+      {"Settings", settings}
+    };
+
+    selection = menu("Goose Game", options, sizeof(options)/sizeof(options[0]), "main");
+
+    if(selection != 0) options[selection-1].callback();
+  }while(selection != 0);
 
   clearConsole();
-
   return 0;
 }
